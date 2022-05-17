d95765009105aa62cc6a77ea33bfef6603e24250
Dmitry Bulykin <dbulyk@icloud.com>
2022-05-17 19:49:17 +0300

    Создано приложение Javalin, подготовка к деплою

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+// Импортируем зависимости, необходимые для работы приложения
+import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
 
+
+public final class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        return Integer.parseInt(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello, world!"));
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(JavalinConfig::enableDevLogging);
+        addRoutes(app);
+
+        app.before(ctx -> ctx.attribute("ctx", ctx));
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
